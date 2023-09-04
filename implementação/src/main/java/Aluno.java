a07fe85b80173091fca8b9b337ce34f33e4c34e7
brunopdt <bruno.pduarte0@gmail.com>
2023-09-04 01:15:05 -0300

    Validação de créditos da matrícula do aluno

--- implementação/src/main/java/Aluno.java
+++ implementação/src/main/java/Aluno.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Aluno extends Usuario {
@@ -18,9 +19,13 @@ public class Aluno extends Usuario {
   };
 
   public void matricular(Turma turma) throws IllegalArgumentException {
-    if (turmas.size() >= MAXIMO_TURMAS) {
+    if (turmas.size() >= MAXIMO_TURMAS)
       throw new IllegalArgumentException("Número máximo de turmas atingido");
-    }
+    else if (turmas.contains(turma))
+      throw new IllegalArgumentException("Aluno já matriculado");
+    else if (this.getCurso().getCreditos() >= this.somaCreditosMatriculados())
+      throw new IllegalArgumentException("Número máximo de créditos atingido");
+
     Historico historico = new Historico(turma, this);
     turmas.add(historico);
     turma.adicionarHistoricos(historico);
@@ -35,6 +40,12 @@ public class Aluno extends Usuario {
       }
     }
   };
+
+  public double somaCreditosMatriculados() {
+    return Arrays.stream(listarTurmasMatriculadas())
+        .mapToDouble(turma -> turma.getDisciplina().getCreditos())
+        .sum();
+  }
 
   public Turma[] listarTurmasMatriculadas() {
     Turma[] turmasMatriculadas = turmas.stream()
